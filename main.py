0889e69487b9c867c829475b46044b08930608ed
Tanmay <findtanmay10@gmail.com>
2025-04-08 23:25:51 +0530

    data storing sorted

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from scapy.all import sniff, wrpcap
-import pandas as pd
+from scapy.all import sniff, wrpcap, IP, TCP, UDP, ICMP
 import os, time, threading, sys
+import csv
+from datetime import datetime
+
+os.makedirs("data/pcap", exist_ok=True)
+os.makedirs("data/txt", exist_ok=True)
+os.makedirs("data/csv", exist_ok=True)
 
 if len(sys.argv) != 3 or sys.argv[1] != "-t":
     print("Usage: python main.py -t <time-to-sniff>")
@@ -40,11 +45,36 @@ sniffThread.join()
 
 wrpcap(f"data/pcap/{name}.pcap", packets)
 
-data = pd.DataFrame(packets)
+with open(f"data/csv/{name}.csv", mode="w", newline="") as file:
+    writer = csv.writer(file)
+    # Write header
+    writer.writerow(
+        ["Src IP", "Src Port", "Dst IP", "Dst Port", "Protocol", "Timestamp"]
+    )
+    for pkt in packets:
+        if IP in pkt:
+            src_ip = pkt[IP].src
+            dst_ip = pkt[IP].dst
 
-data.to_csv(
-    f"data/csv/{name}.csv",
-)
+            proto = "OTHER"
+            src_port = ""
+            dst_port = ""
+
+            if TCP in pkt:
+                proto = "TCP"
+                src_port = pkt[TCP].sport
+                dst_port = pkt[TCP].dport
+            elif UDP in pkt:
+                proto = "UDP"
+                src_port = pkt[UDP].sport
+                dst_port = pkt[UDP].dport
+            elif ICMP in pkt:
+                proto = "ICMP"
+
+            timestamp = datetime.fromtimestamp(float(pkt.time))
+
+            writer.writerow([src_ip, src_port, dst_ip, dst_port, proto, timestamp])
+
 for i, packet in enumerate(packets):
     print(f"Packet{i}:{packet.summary()}")
     src_ip = packet["IP"].src if packet.haslayer("IP") else "N/A"
@@ -57,4 +87,4 @@ for i, packet in enumerate(packets):
         else "UDP" if packet.haslayer("UDP") else "Other"
     )
     with open(f"data/txt/{name}.txt", "a") as n:
-        n.write(f"{src_ip, src_port, dst_ip, dst_port, protocol}\n")
+        n.write(f"{src_ip}:{src_port} -> {dst_ip}:{dst_port} | {protocol}\n")
